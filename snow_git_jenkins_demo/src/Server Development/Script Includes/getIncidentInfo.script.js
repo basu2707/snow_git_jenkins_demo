e9d1e79059973269f5eb94e050079b5225315b99
Basu Velivela <bveliv410@comcast.com>
2021-03-04 14:44:33 -0500

    changes

--- snow_git_jenkins_demo/src/Server Development/Script Includes/getIncidentInfo.script.js
+++ snow_git_jenkins_demo/src/Server Development/Script Includes/getIncidentInfo.script.js
@@ -1,7 +1,8 @@
 var getIncidentInfo = Class.create();
 getIncidentInfo.prototype = {
     initialize: function() {
-		gs.log("Test Simple Script Include from Studio development");
+		  gs.log("Test Simple Script Include from Studio development");
+      //Updated comment in VS Code
     },
 
     type: 'getIncidentInfo'
