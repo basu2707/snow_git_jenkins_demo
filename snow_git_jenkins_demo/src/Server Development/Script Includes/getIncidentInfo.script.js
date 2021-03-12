8e3a0c2f98b008331e3f4bee499fd9d52ef661e2
Basu Velivela <bveliv410@comcast.com>
2021-03-12 14:36:13 -0500

    Update from VSCode

--- snow_git_jenkins_demo/src/Server Development/Script Includes/getIncidentInfo.script.js
+++ snow_git_jenkins_demo/src/Server Development/Script Includes/getIncidentInfo.script.js
@@ -3,6 +3,7 @@ getIncidentInfo.prototype = {
     initialize: function() {
 		  gs.log("Test Simple Script Include from Studio development");
       //Updated comment in VS Code
+      gs.log("Update from VSCode");
     },
 
     type: 'getIncidentInfo'
